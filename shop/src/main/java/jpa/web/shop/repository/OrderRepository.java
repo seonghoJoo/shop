cf31395d67fcf43eb01fe364cf2c246e576bb798
seonghojoo <etkl8888@naver.com>
2022-02-27 23:38:48 +0900

    UPDATE QueryDSL 추가

--- shop/src/main/java/jpa/web/shop/repository/OrderRepository.java
+++ shop/src/main/java/jpa/web/shop/repository/OrderRepository.java
@@ -1,6 +1,8 @@
 package jpa.web.shop.repository;
 
-import jpa.web.shop.domain.Member;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jpa.web.shop.domain.*;
 import jpa.web.shop.domain.Order;
 import jpa.web.shop.repository.order.simplequery.SimpleOrderQueryDto;
 import lombok.RequiredArgsConstructor;
@@ -39,6 +41,33 @@ public class OrderRepository {
 //                .getResultList();
 //    }
     // queryDsl로 처리
+
+    public List<Order> findAll(OrderSearch orderSearch){
+        JPAQueryFactory query = new JPAQueryFactory(em);
+        QOrder order = QOrder.order;
+        QMember member = QMember.member;
+
+        return query.select(order)
+                .from(order)
+                .join(order.member, member)
+                .where(statusEq(orderSearch.getOrderStatus()), nameLike(orderSearch.getMemberName()))
+                .limit(1000)
+                .fetch();
+    }
+
+    private BooleanExpression nameLike(String memberName){
+        if(!StringUtils.hasText(memberName)){
+            return null;
+        }
+        return QMember.member.username.like(memberName);
+    }
+
+    private BooleanExpression statusEq(OrderStatus statusCond){
+        if(statusCond == null){
+            return null;
+        }
+        return QOrder.order.status.eq(statusCond);
+    }
 
     public List<Order> findAllByCriteria(OrderSearch orderSearch) {
         CriteriaBuilder cb = em.getCriteriaBuilder();
